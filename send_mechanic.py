4c35ca7faf187ae6a494e993f223f7efa9699a07
MohnishSS <133986129+MohnishSS@users.noreply.github.com>
2024-11-03 01:22:50 -0500

    Delete send_mechanic.py

--- send_mechanic.py
+++
@@ -1,4 +0,0 @@
-account_sid = 'ACef43fdffffcb2378f72d4e0b0fb37878'
-auth_token = '4b4c3d81688e3b560bebd26e1030cb64'
-from_whatsapp_number = 'whatsapp:+14155238886'
-to_whatsapp_number = 'whatsapp:+15088166235'
