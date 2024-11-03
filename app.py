c197d92a9f5e1593a28205e50be1641acfd85847
MohnishSS <mohnishshridhar@gmail.com>
2024-11-03 00:50:59 -0400

    final project i think

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ app = Flask(__name__)
 
 DATA_FILE = "data.json"
 
-#client = Client(send_mechanic.account_sid, send_mechanic.auth_token)
+client = Client(send_mechanic.account_sid, send_mechanic.auth_token)
 
 # Load data from JSON file
 def load_data():
     try:
         with open(DATA_FILE, "r") as file:
             data = json.load(file)
-            print("Data loaded:", data)  # Debugging statement
+            #print("Data loaded:", data)  # Debugging statement
             return data
     except FileNotFoundError:
         return {"users": []}
@@ -53,19 +53,41 @@ def log_dose(user_id):
         dosage_date = date.today().strftime("%m-%d-%Y")
 
         # Judges: We are so proud of this code! 
-        # it determines whether a taken dose is late
+        # it determines whether a taken dose is late or an overdose
+        
+        overdoseBool=False
+        dose_count=0
+        maxDoses=0
+        print("MED NAME = ", medication_name)
+        for med in user["medications"]:
+            print(med)
+            if med["name"]==medication_name:
+                maxDoses=len(med["dosage_times"])
+                print("MAXDOSES = ", maxDoses)
+                break
+        for dose in user["log"]:
+            if dose['name']==medication_name:
+                dose_count+=1
+        print("DOSECOUNT = ", dose_count)
+        if dose_count>maxDoses:
+            overdoseBool=True
+            print("NOW ITS AN OVERDOSE")
+            return redirect(url_for("explain", user_id=user_id, reason="overdose", med_name = medication_name))
+        
         lateBool = False
         dosage_timeInMin = int(dosage_time[:2])*60 + int(dosage_time[3:])
         for i in user["medications"]:
             if i["name"] == medication_name:
+                mins = []
                 for j in i["dosage_times"]:
-                    mins = []
                     distExpectedAndReal = abs(int(j[:2])*60 + int(j[3:])- dosage_timeInMin)
+                    #print("THIS IS THE VALUE: !!!!!!!!! ", distExpectedAndReal)
                     mins.append(distExpectedAndReal)
+                    #print(min(mins))
                 if min(mins) > 59:
                     lateBool = True
+                    return redirect(url_for("explain", user_id=user_id, reason="late", med_name=medication_name))
         
-        print(lateBool)
 
         # Log the dose
         user["log"].append({
@@ -75,9 +97,8 @@ def log_dose(user_id):
         })
         save_data(data)
         
-        if lateBool:
-            return redirect(url_for("explain", user_id=user_id, reason="late", med_name = medication_name))
-        else:
+        
+        if not overdoseBool and not lateBool:
             return redirect(url_for("home"))
     
     return render_template("log_dose.html", user=user, current_time=datetime.now().strftime("%H:%M"))
@@ -91,29 +112,7 @@ def compliance(user_id):
     
     if not user:
         return "User not found", 404
-    
-    alerts = []
-    medication_count = {}
-    
-    for dose in user["log"]:
-        #print(f"{dose['name']}_{dose['dosage_date']}")
-        date_key = f"{dose['name']}_{dose['dosage_date']}"
-        if date_key in medication_count:
-            medication_count[date_key] += 1
-        else:
-            medication_count[date_key] = 1
-            
-    for med_date, count in medication_count.items():
-        if count > 1:
-            medication_name, dosage_date = med_date.split('_')
-            alerts.append(f"Alert: Multiple doses of {medication_name} logged on {dosage_date}. Total: {count}.")
-            # client.messages.create(body=f"This is a notification to inform you that your loved one ___ has not been following their prescription routine given by their primary care provider. Please get in touch with them. Alert: Multiple doses of {medication_name} logged on {dosage_date}. Total: {count}.",
-            #         from_=send_mechanic.from_whatsapp_number,
-            #         to=send_mechanic.to_whatsapp_number)
-            
-    
 
-    
     if user["communication"] is not None:
         date = user["communication"][0]
         msg = user["communication"][1]
@@ -121,13 +120,27 @@ def compliance(user_id):
         date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  
         msg = "No messages available."
 
-    return render_template("compliance.html", user=user, alerts=alerts, date=date, msg=msg)  # Only pass the user data
+    return render_template("compliance.html", user=user, date=date, msg=msg)  # Only pass the user data
 
-@app.route("/explain/<int:user_id>/<reason>/<med_name>")
+@app.route("/explain/<int:user_id>/<reason>/<med_name>", methods=["GET", "POST"])
 def explain(user_id, reason, med_name):
     data = load_data()
     if reason=="late":
         alert_message=f"You took {med_name} on the wrong time. We are sending an alert to your emergency contact and your doctor."
+    elif reason=="overdose":
+        alert_message=f"You took {med_name} more than you were supposed to today. We are sending an alert to your emergency contact and your doctor."
+    
+    if request.method == "POST":
+        reason_message = request.form["message"]
+        if reason=="overdose":
+            client.messages.create(body=f"This is a notification to inform you that your loved one has not been following their prescription routine given by their primary care provider. Please get in touch with them. Alert: Possible overdose of {med_name}. Here is what he said: {reason_message}",
+                        from_=send_mechanic.from_whatsapp_number,
+                        to=send_mechanic.to_whatsapp_number)
+        elif reason=="late":
+            client.messages.create(body=f"This is a notification to inform you that your loved one has not been following their prescription routine given by their primary care provider. Please get in touch with them. Alert: Missed the schedule for {med_name}. Here is what he said: {reason_message}",
+                        from_=send_mechanic.from_whatsapp_number,
+                        to=send_mechanic.to_whatsapp_number)
+        return redirect(url_for("home"))
     return render_template("explain.html", users=data["users"], alert=alert_message)
 
 if __name__ == "__main__":
--- send_mechanic.py
+++ send_mechanic.py
@@ -8,7 +8,7 @@ account_sid = 'ACef43fdffffcb2378f72d4e0b0fb37878'
 
 # this is the authentication token of the user account
 auth_token = '4b4c3d81688e3b560bebd26e1030cb64'
-client = Client(account_sid, auth_token)
+
 
 # this is the phone from which the web application will send the notification from
 from_whatsapp_number = 'whatsapp:+14155238886'
@@ -17,6 +17,3 @@ from_whatsapp_number = 'whatsapp:+14155238886'
 # the receiving side phone number
 to_whatsapp_number = 'whatsapp:+15088166235'
 
-client.messages.create(body=f"This is a notification to inform you that your loved one ___ has not been following their prescription routine given by their primary care provider. Please get in touch with them. Alert: Multiple doses of MEDICATION NAME logged on DOSAGE DATE. Total: COUNT.",
-                    from_=from_whatsapp_number,
-                    to=to_whatsapp_number)
