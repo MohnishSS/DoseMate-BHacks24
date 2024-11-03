7bb1dbbf920644988c2b52ded111195effd20208
MohnishSS <mohnishshridhar@gmail.com>
2024-11-03 01:05:19 -0400

    gitignore fixed i think

--- send_mechanic.py
+++ send_mechanic.py
@@ -1,19 +1,4 @@
-from twilio.rest import Client
-
-
-
-# this is the user account id
 account_sid = 'ACef43fdffffcb2378f72d4e0b0fb37878'
-
-
-# this is the authentication token of the user account
 auth_token = '4b4c3d81688e3b560bebd26e1030cb64'
-
-
-# this is the phone from which the web application will send the notification from
 from_whatsapp_number = 'whatsapp:+14155238886'
-
-
-# the receiving side phone number
 to_whatsapp_number = 'whatsapp:+15088166235'
-
